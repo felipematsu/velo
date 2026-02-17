0c12391a8720cf6f6894e49da40b4baa30d0767a
Felipe Matsumoto <35380252+felipematsu@users.noreply.github.com>
2026-02-17 20:58:21 -0300

    removendo test-ids de resultados das buscas

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -8,17 +8,16 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
   await page.goto('http://localhost:5173/');
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
 
-  //a[text()='Consultar Pedido'] - xpath
-  // a[href='/lookup'] - css selector
-  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click();    // a[href='/lookup'] - css selector    //a[text()='Consultar Pedido'] - xpath
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
 
   // Act
-  await page.getByTestId('search-order-id').fill('VLO-680LJF');
-  await page.getByTestId('search-order-button').click();
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-680LJF'); // xpath - //label[text()='Número do Pedido']/..//input
+  // await page.getByTestId('search-order-button').click();
+  await page.getByRole('button', { name: 'Buscar Pedido' }).click();
 
   // Assert
-  await expect(page.getByTestId('order-result-id')).toBeVisible();
+  await expect(page.getByTestId('order-result-id')).toBeVisible({ timeout: 30000 });
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-680LJF');
   await expect(page.getByTestId('order-result-status')).toBeVisible();
   await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
