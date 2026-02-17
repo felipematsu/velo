b6d574cbff3bb93d1e805d59caa48760dc373f58
Felipe Matsumoto <35380252+felipematsu@users.noreply.github.com>
2026-02-17 17:50:51 -0300

    AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,23 +1,23 @@
 import { test, expect } from '@playwright/test';
 
-test('deve consultar um pedido aprovado', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
+// AAA - Arrange, Act, Assert
+// PAV - Preparar, Agir, Verificar
 
-  // Checkpoint
+test('deve consultar um pedido aprovado', async ({ page }) => {
+  // Arrange
+  await page.goto('http://localhost:5173/');
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
 
   //a[text()='Consultar Pedido'] - xpath
   // a[href='/lookup'] - css selector
   await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-
-  // Checkpoint
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
 
+  // Act
   await page.getByTestId('search-order-id').fill('VLO-680LJF');
-
   await page.getByTestId('search-order-button').click();
 
-  // Checkpoint
+  // Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible();
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-680LJF');
   await expect(page.getByTestId('order-result-status')).toBeVisible();
