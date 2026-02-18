65afbd7586004e4fea595a096e0fa50a75cc92d4
Felipe Matsumoto <35380252+felipematsu@users.noreply.github.com>
2026-02-18 17:02:47 -0300

    adicionando progresso

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -16,7 +16,7 @@ test.describe('Consulta de pedido', () => {
       })
       
 
-    test('deve consultar um pedido aprovado', async ({ page }) => {
+      test('deve consultar um pedido aprovado', async ({ page }) => {
 
         // Test Data
         // const order = 'VLO-680LJF';
@@ -56,8 +56,9 @@ test.describe('Consulta de pedido', () => {
           - img
           - paragraph: Pedido
           - paragraph: ${order.number}
-          - img
-          - text: ${order.status}
+          - status:
+            - img
+            - text: ${order.status}
           - img "Velô Sprint"
           - paragraph: Modelo
           - paragraph: Velô Sprint
@@ -80,7 +81,14 @@ test.describe('Consulta de pedido', () => {
           - paragraph: ${order.payment}
           - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
           `);
-      
+
+        const statusBadge = page.getByRole('status').filter({hasText: order.status})
+          
+        await expect(statusBadge).toHaveClass(/bg-green-100/);
+        await expect(statusBadge).toHaveClass(/text-green-700/);
+
+        const statusIcon = statusBadge.locator('svg');
+        await expect(statusIcon).toHaveClass(/lucide-check-circle/); 
       //   await expect(page.getByTestId('order-result-id')).toBeVisible({ timeout: 30000 });
       //   await expect(page.getByTestId('order-result-id')).toContainText('VLO-680LJF');
       //   await expect(page.getByTestId('order-result-status')).toBeVisible();
@@ -112,8 +120,9 @@ test.describe('Consulta de pedido', () => {
           - img
           - paragraph: Pedido
           - paragraph: ${order.number}
-          - img
-          - text: ${order.status}
+          - status:
+            - img
+            - text: ${order.status}
           - img "Velô Sprint"
           - paragraph: Modelo
           - paragraph: Velô Sprint
@@ -136,6 +145,73 @@ test.describe('Consulta de pedido', () => {
           - paragraph: ${order.payment}
           - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
           `);
+
+          const statusBadge = page.getByRole('status').filter({hasText: order.status})
+          
+          await expect(statusBadge).toHaveClass(/bg-red-100/);
+          await expect(statusBadge).toHaveClass(/text-red-700/);
+  
+          const statusIcon = statusBadge.locator('svg');
+          await expect(statusIcon).toHaveClass(/lucide-x-circle/); 
+      });
+
+      test('deve consultar um pedido em análise', async ({ page }) => {
+
+        // Test Data
+        const order = {
+          number: 'VLO-F8U4SU',
+          status: 'EM_ANALISE',
+          color: 'Lunar White',
+          wheels: 'aero Wheels',
+          customer: {
+            name: 'joao da silva',
+            email: 'joao@velo.dev'
+          },
+          payment: 'À Vista'
+        }
+
+        // Act
+        await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order.number); // xpath - //label[text()='Número do Pedido']/..//input
+        await page.getByRole('button', { name: 'Buscar Pedido' }).click();
+      
+        // Assert
+        await expect(page.getByTestId(`order-result-${order.number}`)).toMatchAriaSnapshot(`
+          - img
+          - paragraph: Pedido
+          - paragraph: ${order.number}
+          - status:
+            - img
+            - text: ${order.status}
+          - img "Velô Sprint"
+          - paragraph: Modelo
+          - paragraph: Velô Sprint
+          - paragraph: Cor
+          - paragraph: ${order.color}
+          - paragraph: Interior
+          - paragraph: cream
+          - paragraph: Rodas
+          - paragraph: ${order.wheels}
+          - heading "Dados do Cliente" [level=4]
+          - paragraph: Nome
+          - paragraph: ${order.customer.name}
+          - paragraph: Email
+          - paragraph: ${order.customer.email}
+          - paragraph: Loja de Retirada
+          - paragraph
+          - paragraph: Data do Pedido
+          - paragraph: /\\d+\\/\\d+\\/\\d+/
+          - heading "Pagamento" [level=4]
+          - paragraph: ${order.payment}
+          - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+          `);
+
+          const statusBadge = page.getByRole('status').filter({hasText: order.status})
+          
+          await expect(statusBadge).toHaveClass(/bg-amber-100/);
+          await expect(statusBadge).toHaveClass(/text-amber-700/);
+  
+          const statusIcon = statusBadge.locator('svg');
+          await expect(statusIcon).toHaveClass(/lucide-clock/); 
       });
       
       test('deve exibir mensagem quando o pedido não é encontrado', async ({ page }) => {
