7210ef24916ba5ffd97eab5cafb7ba12377db953
Felipe Matsumoto <35380252+felipematsu@users.noreply.github.com>
2026-04-21 14:58:08 -0300

    editando playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,13 +16,15 @@ dotenv.config({ path: path.resolve(__dirname, '.env') });
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  // Tempo máximo para cada teste completo (30 segundos padrão)
+
+  // Tempo máximo para cada teste completo (3o segundo é o padrão)
   timeout: 60_000,
 
-  // Tempo máximo para assertions (toBeVisible, toContainText, etc.) (5 segundos padrão)
+  // Tempo máximo para assertions (toBeVisible(), toHaveText()) 5 segundos
   expect: {
-    timeout: 5_000, // não vale a pena aumentar porque o teste pode ficar lento no tempo de execução, vale a pena usar o timeout explicítos
+    timeout: 5_000 // não vale a pena aumentar porque o teste pode ficar lento no tempo de execução, vale a pena usar o time explicito
   },
+
 
   testDir: './playwright/e2e',
   /* Run tests in files in parallel */
@@ -37,17 +39,17 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    baseURL: 'http://localhost:5173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'retain-on-failure',
+    trace: 'on',
 
-    // Tempo máximo para ações interativas como click, fill
+    // Tempo máximo para ações interativas como click(), fill()
     // Quando o valor é 0, herda o limite do timeout geral do teste
     actionTimeout: 5_000,
 
-    // Tempo máximo para navegações como goto, waitForURL
+    // Tempo máximo para navegações como goto(), waitForURL()
     // Quando o valor é 0, herda o limite do timeout geral do teste
     navigationTimeout: 10_000
   },
