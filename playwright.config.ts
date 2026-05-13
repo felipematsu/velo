7ca1474beae0c23917bb4d09f8fcf67644f2eeb3
Felipe Matsumoto <35380252+felipematsu@users.noreply.github.com>
2026-05-13 12:40:10 -0300

    adicionando progresso da aula: Deploy Manual com Vercel CLI e Testes em Preview

--- playwright.config.ts
+++ playwright.config.ts
@@ -40,7 +40,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://velo-felipematsu.vercel.app',
+    baseURL: 'https://velo-2n805pgj9-felipe-matsumoto-s-projects.vercel.app/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
